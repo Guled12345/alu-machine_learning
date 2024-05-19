febf81976bc300cbb951e814c56ee468749c964f
Guled12345 <you@example.com>
2024-05-19 22:09:17 +0300

    0-slice_me_up.py

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python3
-# Define the array
 arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
-# Slice the array to get the first two numbers
 arr1 = arr[0:2]
-# Slice the array to get the last five numbers
 arr2 = arr[4:]
-# Slice the array to get the numbers from the 2nd through 6th positions
 arr3 = arr[1:6]
-# Print the results
 print("The first two numbers of the array are: {}".format(arr1))
 print("The last five numbers of the array are: {}".format(arr2))
 print("The 2nd through 6th numbers of the array are: {}".format(arr3))
-
