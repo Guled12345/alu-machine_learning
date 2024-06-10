f1772f03b08b08faea6cecca52d2beac3654e074
Guled12345 <you@example.com>
2024-06-10 12:35:12 +0300

    normal.py

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """Create a class Normal that represents a normal distribution"""
 
-
 class Normal:
     """class Normal"""
-    
+
     def __init__(self, data=None, mean=0., stddev=1.):
         """Constructor"""
         self.E = 2.7182818285
