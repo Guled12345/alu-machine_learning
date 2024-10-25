75cbe1b7bebd17ae8274edb53fbb02ea959693a6
Guled12345 <g.warsameh@alustudent.com>
2024-10-25 19:53:53 +0300

    2-user_location.py

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,34 +1,49 @@
 #!/usr/bin/env python3
+'''
+Prints the location of a user
+'''
 
-"""
-Using the GitHub API, this script prints the location of a specific user.
-"""
 
-import requests
 import sys
+import requests
 import time
 
-if __name__ == "__main__":
-    # Ensure the user provides the correct argument
-    if len(sys.argv) < 2:
-        print("Usage: ./2-user_location.py <GitHub API URL>")
+
+def get_user_location(api_url):
+    """
+    Fetch and print the location of a GitHub user.
+
+    :param api_url: The API URL for the user
+    """
+    try:
+        response = requests.get(api_url)
+
+        if response.status_code == 200:
+            user_data = response.json()
+            location = user_data.get('location')
+            if location:
+                print(location)
+            else:
+                print('Location not available')
+        elif response.status_code == 404:
+            print('Not found')
+        elif response.status_code == 403:
+            reset_time = int(
+                response.headers.get('X-RateLimit-Reset', time.time()))
+            current_time = int(time.time())
+            wait_time = (reset_time - current_time) // 60
+            print('Reset in {} min'.format(wait_time))
+        else:
+            print('Error: {}'.format(response.status_code))
+    except requests.RequestException as e:
+        print('An error occurred: {}'.format(e))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: ./2-user_location.py <api_url>')
         sys.exit(1)
 
-    # Make a request to the provided URL
-    res = requests.get(sys.argv[1])
-
-    if res.status_code == 403:
-        # Handle rate limit error
-        rate_limit = int(res.headers.get('X-Ratelimit-Reset', 0))
-        current_time = int(time.time())
-        diff = (rate_limit - current_time) // 60
-        print(f"Reset in {diff} min")
-
-    elif res.status_code == 404:
-        # Handle user not found
-        print("Not found")
-
-    elif res.status_code == 200:
-        # Print the user's location
-        data = res.json()
-        print(data.get('location', 'Location not available'))
+    api_url = sys.argv[1]
+    get_user_location(api_url)
+    
